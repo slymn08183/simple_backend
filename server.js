3db4b6bc3a8b3ee85bd2a9473f98d55f31bf2127
slymn08183 <slymn08183@gmail.com>
2021-02-03 23:58:00 +0300

    basic get & post

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 
 const PORT = 5000;
 
+app.get("/get",(req,res,next) =>
+{
+    res.json({"a": "bc"})
+})
+
+app.post("/post",(req,res,next) =>
+{
+    res.json({"a": "bpostc"})
+})
+
 app.listen(PORT, () => {
     console.log("Server Started At : " + PORT);
 })
